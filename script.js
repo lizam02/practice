133c85d737e5ddf366372ff8d54c157eb4baf4f9
lizam02 <liza.merkulova2005@yandex.ru>
2022-06-02 18:19:24 +0300

    lesson_3

---
+++ script.js
@@ -0,0 +1,28 @@
+function guess() {
+    var guessNumber = 5
+    var ourNumber = prompt("Введите число");
+
+    if (ourNumber > guessNumber) {
+        alert("Число должно быть меньше");
+        return guess();
+    } else if (ourNumber == guessNumber) {
+        return alert("Вы угадали!");
+    } else {
+        alert("Число должно быть больше");
+        return guess();
+    }
+}
+
+//function reminder () {
+    alert("Ты здесь слишком долго!");
+    //window.close();
+
+
+//setTimeout(reminder, 3000);
+
+function valideForm() {
+    alert("Проверка формы");
+    var name = document.getElementById("name").value;
+    console.log(name);
+}
+document.querySelector(".button").addEventListener("click", valideForm);
